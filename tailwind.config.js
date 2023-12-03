71c263e33b58bc1c707e769064e8904aff0f9fb3
Aryan Kumar <aryanajha256@gmail.com>
2023-12-04 05:14:13 +0530

    Remove unused code and add decorator background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      animation: {
-        "spin-slow": "spin 3s linear infinite",
-        "translate-x-2": "step 2s linear infinite",
-      },
-      keyframes: {
-        spin: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(-360deg)" },
-        },
-        "translate-x": {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
