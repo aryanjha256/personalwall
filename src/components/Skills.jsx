7f8bc97d1f04eb7af18212f0709402e9666875f4
Aryan Kumar <aryanajha256@gmail.com>
2023-12-03 13:35:19 +0530

    Update Skills component and index.css

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,9 +5,11 @@ import { skills } from "../data/skills";
 const Skills = () => {
   return (
     <div className="bg-neutral-900 py-12 border-y border-neutral-800 mt-6">
-      <div className="flex items-center justify-evenly overflow-auto">
+      <div className="flex items-center justify-evenly overflow-clip whitespace-nowrap">
         {skills.map((skill, index) => (
-          <div key={index}>{skill.icon}</div>
+          <div key={index} className="inline-block mx-10">
+            {skill.icon}
+          </div>
         ))}
       </div>
     </div>
