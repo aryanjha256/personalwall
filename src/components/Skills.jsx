2b11a440b334ebae858f6816f4ded56c422751b8
Aryan Kumar <aryanajha256@gmail.com>
2023-12-03 14:14:25 +0530

    Refactor Skills component and update Tailwind CSS

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import { skills } from "../data/skills";
 
 const Skills = () => {
@@ -7,7 +5,7 @@ const Skills = () => {
     <div className="bg-neutral-900 py-12 border-y border-neutral-800 mt-6">
       <div className="flex items-center justify-evenly overflow-clip whitespace-nowrap">
         {skills.map((skill, index) => (
-          <div key={index} className="inline-block mx-10">
+          <div key={index} className="inline-block mx-8 md:mx-10 lg:mx-12">
             {skill.icon}
           </div>
         ))}
